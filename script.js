80a1afffed2f7a150a1fed932a50ccc04f0013f9
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-10-07 23:11:34 +0530

    Started the project

---
+++ script.js
@@ -0,0 +1,27 @@
+var game = {
+    user: '',
+    computer: '',
+    currentPlayer: '',
+    moves: 1,
+  };
+
+function setFig(id) {
+    if (id === 'x') {
+      game.user = '<span class="fa fa-times"></span>';
+      game.computer = '<span class="fa fa-circle-o"></span>';
+    } else if (id === 'o') {
+      game.user = '<span class="fa fa-circle-o"></span>';
+      game.computer = '<span class="fa fa-times"></span>';
+    }
+    firstMove();
+    setCurrPl('user');
+}
+
+function firstMove() {
+  $('#fifth').html(game.computer);
+  $('#fifth').removeAttr('onClick');
+}
+
+function setCurrPl(curr) {
+    game.currentPlayer = curr;
+}
